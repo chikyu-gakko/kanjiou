fc2d0f1baf6681260ff63a167eb1b4f4af0186c7
s61t6ftaro <bisk1280392@gn.iwasaki.ac>
2021-12-01 00:39:32 +0900

    window add start text

--- js/open_logo.js
+++ js/open_logo.js
@@ -3,30 +3,53 @@ export default class OpenLogo extends Phaser.Scene {
     super({ key: "logo", active: true });
   }
 
+  init(){
+    this.fontFamily = this.registry.get("fontFamily");
+  }
+
   preload() {
     this.load.path = window.location.href.replace("index.html", "");
     // ロゴマーク
     this.load.image("logo", "img/logo_tikyu.png");
   }
-
+  
   create() {
     // 画像表示
-
+    
     // logo画像
     const logoImage = this.add.image(500, 400, "logo");
-    logoImage.depth = 1;
-
+    
+    this.add.text(
+      this.game.canvas.width / 2,600,"画面をタッチしてスタート！",
+      {
+        fill: "#ffff",
+        fontFamily: this.fontFamily,
+        fontSize: "32px",
+      }
+    )
+    .setOrigin(0.5, 0);
+    
     // fadein/out
-
     this.cameras.main.fadeIn(2000);
     this.cameras.main.once("camerafadeincomplete", (camera) => {
-      camera.fadeOut(2000);
-    });
+      
 
-    this.cameras.main.once("camerafadeoutcomplete", () => {
-      this.scene.start("game_menu");
-    });
-
+      this.add
+        .graphics()
+        .fillStyle(0x000000, 0.05)
+        .fillRect(0,0,this.game.canvas.width, this.game.canvas.height)
+        .setInteractive(
+          new Phaser.Geom.Rectangle(0,0, this.game.canvas.width, this.game.canvas.height),
+          Phaser.Geom.Rectangle.Contains
+          )
+          
+          .on('pointerdown',()=>{
+            camera.fadeOut(2000);
+            this.scene.start("game_menu");
+          });
+      
+      });
+        
     this.tweens.add({
       targets: logoImage,
       ease: "Sine.easeInOut",
