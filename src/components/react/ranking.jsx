3377f6fcff5b99d650280daaddf64859f8501c74
tamagram <tamadevelopment211@gmail.com>
2022-11-30 09:01:53 +0900

    :boom: Discard the project once

---
+++ src/components/react/ranking.jsx
@@ -0,0 +1,64 @@
+import React from "react"
+
+const Ranking = () => {
+  return (
+    <>
+      <div id="headline">
+        <img src="../assets/img/clown.png" />
+        <h1>ランキング</h1>
+      </div>
+
+      <div className="menu-bth">
+        <div className="bth1">
+          <img src="../assets/img/dog_black.png" />
+          <div>羊の中に犬1匹</div>
+        </div>
+        <div className="bth1">
+          <img src="../assets/img/world.png" />
+          <div>世界の文字</div>
+        </div>
+        <div className="bth1">
+          <img src="../assets/img/tramp.png" />
+          <div>神経衰弱</div>
+        </div>
+        <div className="bth1">
+          <img src="../assets/img/member.png" />
+          <div>仲間で集まれ</div>
+        </div>
+        <div className="bth1">
+          <img id="modura" src="../assets/img/no_shadow.png" />
+          <div>チャレンジモード</div>
+        </div>
+      </div>
+
+      <div id="dashboard">
+        <div className="left">
+          <div id="left-fonts">
+            <div>制限時間</div>
+            <div>タイムアタック</div>
+          </div>
+        </div>
+        <div className="right">
+          <div className="bar">
+            <div className="bar1">順位</div>
+            <div className="bar1">名前</div>
+            <div className="bar1">時間</div>
+            <div className="bar1">日付</div>
+          </div>
+          <table id="sample"></table>
+          <div className="under_bar">
+            <div id="under_bar_content">
+              <button id="back_bth">←戻る</button>
+              <div id="under_bar_content1">
+                <img src="../assets/img/challengeMode.svg" />
+                <p id="under_bar_label">順位のTOP100までが表示されるよ！</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Ranking
