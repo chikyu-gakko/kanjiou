3a64c7f910f2a2c769efde383f5ace10bdeff0f2
八尋 諒 <yahiroryo@email.yappi.jp>
2024-03-13 13:41:09 +0900

    data投入

--- src/nakamaStrokeCountData.js
+++ src/nakamaStrokeCountData.js
@@ -325,166 +325,627 @@ export const nakamaStrokeCountData = [
     ],
     [
         {
-            'strokeCount': 4,
-            'correctValues': ['化']
+            strokeCount: 4,
+            correctValues: ['化']
         },
         {
-            'strokeCount': 5,
-            'correctValues': ['代']
+            strokeCount: 5,
+            correctValues: ['代']
         },
         {
-            'strokeCount': 4,
-            'correctValues': ['反']
+            strokeCount: 4,
+            correctValues: ['反']
         },
         {
-            'strokeCount': 5,
-            'correctValues': ['皮']
+            strokeCount: 5,
+            correctValues: ['皮']
         },
         {
-            'strokeCount': 5,
-            'correctValues': ['皿']
+            strokeCount: 5,
+            correctValues: ['皿']
         },
         {
-            'strokeCount': 6,
-            'correctValues': ['血']
+            strokeCount: 6,
+            correctValues: ['血']
         },
         {
-            'strokeCount': 5,
-            'correctValues': ['由']
+            strokeCount: 5,
+            correctValues: ['由']
         },
         {
-            'strokeCount': 6,
-            'correctValues': ['曲']
+            strokeCount: 6,
+            correctValues: ['曲']
         },
         {
-            'strokeCount': 7,
-            'correctValues': ['住']
+            strokeCount: 7,
+            correctValues: ['住']
         },
         {
-            'strokeCount': 9,
-            'correctValues': ['柱']
+            strokeCount: 9,
+            correctValues: ['柱']
         },
         {
-            'strokeCount': 7,
-            'correctValues': ['坂']
+            strokeCount: 7,
+            correctValues: ['坂']
         },
         {
-            'strokeCount': 8,
-            'correctValues': ['板']
+            strokeCount: 8,
+            correctValues: ['板']
         },
         {
-            'strokeCount': 9,
-            'correctValues': ['重']
+            strokeCount: 9,
+            correctValues: ['重']
         },
         {
-            'strokeCount': 12,
-            'correctValues': ['軽']
+            strokeCount: 12,
+            correctValues: ['軽']
         },
         {
-            'strokeCount': 10,
-            'correctValues': ['流']
+            strokeCount: 10,
+            correctValues: ['流']
         },
         {
-            'strokeCount': 11,
-            'correctValues': ['深']
+            strokeCount: 11,
+            correctValues: ['深']
         },
         {
-            'strokeCount': 10,
-            'correctValues': ['倍']
+            strokeCount: 10,
+            correctValues: ['倍']
         },
         {
-            'strokeCount': 11,
-            'correctValues': ['部']
+            strokeCount: 11,
+            correctValues: ['部']
         },
         {
-            'strokeCount': 9,
-            'correctValues': ['負']
+            strokeCount: 9,
+            correctValues: ['負']
         },
         {
-            'strokeCount': 10,
-            'correctValues': ['員']
+            strokeCount: 10,
+            correctValues: ['員']
         },
         {
-            'strokeCount': 8,
-            'correctValues': ['岸']
+            strokeCount: 8,
+            correctValues: ['岸']
         },
         {
-            'strokeCount': 9,
-            'correctValues': ['炭']
+            strokeCount: 9,
+            correctValues: ['炭']
         },
         {
-            'strokeCount': 11,
-            'correctValues': ['第']
+            strokeCount: 11,
+            correctValues: ['第']
         },
         {
-            'strokeCount': 12,
-            'correctValues': ['等']
+            strokeCount: 12,
+            correctValues: ['等']
         },
         {
-            'strokeCount': 9,
-            'correctValues': ['度']
+            strokeCount: 9,
+            correctValues: ['度']
         },
         {
-            'strokeCount': 10,
-            'correctValues': ['庭']
+            strokeCount: 10,
+            correctValues: ['庭']
         },
         {
-            'strokeCount': 11,
-            'correctValues': ['問']
+            strokeCount: 11,
+            correctValues: ['問']
         },
         {
-            'strokeCount': 12,
-            'correctValues': ['開']
+            strokeCount: 12,
+            correctValues: ['開']
         },
         {
-            'strokeCount': 11,
-            'correctValues': ['動']
+            strokeCount: 11,
+            correctValues: ['動']
         },
         {
-            'strokeCount': 12,
-            'correctValues': ['運']
+            strokeCount: 12,
+            correctValues: ['運']
         },
         {
-            'strokeCount': 10,
-            'correctValues': ['旅']
+            strokeCount: 10,
+            correctValues: ['旅']
         },
         {
-            'strokeCount': 11,
-            'correctValues': ['族']
+            strokeCount: 11,
+            correctValues: ['族']
         },
         {
-            'strokeCount': 9,
-            'correctValues': ['発']
+            strokeCount: 9,
+            correctValues: ['発']
         },
         {
-            'strokeCount': 12,
-            'correctValues': ['登']
+            strokeCount: 12,
+            correctValues: ['登']
         },
         {
-            'strokeCount': 12,
-            'correctValues': ['悲']
+            strokeCount: 12,
+            correctValues: ['悲']
         },
         {
-            'strokeCount': 13,
-            'correctValues': ['想']
+            strokeCount: 13,
+            correctValues: ['想']
         },
         {
-            'strokeCount': 11,
-            'correctValues': ['悪']
+            strokeCount: 11,
+            correctValues: ['悪']
         },
         {
-            'strokeCount': 13,
-            'correctValues': ['感']
+            strokeCount: 13,
+            correctValues: ['感']
         },
         {
-            'strokeCount': 13,
-            'correctValues': ['鉄']
+            strokeCount: 13,
+            correctValues: ['鉄']
         },
         {
-            'strokeCount': 14,
-            'correctValues': ['銀']
+            strokeCount: 14,
+            correctValues: ['銀']
         }
     ],
-    
+    [
+        {
+            strokeCount: 4,
+            correctValues: ['氏']
+        },
+        {
+            strokeCount: 5,
+            correctValues: ['史']
+        },
+        {
+            strokeCount: 5,
+            correctValues: ['以']
+        },
+        {
+            strokeCount: 6,
+            correctValues: ['衣']
+        },
+        {
+            strokeCount: 7,
+            correctValues: ['芸']
+        },
+        {
+            strokeCount: 8,
+            correctValues: ['英']
+        },
+        {
+            strokeCount: 7,
+            correctValues: ['囲']
+        },
+        {
+            strokeCount: 8,
+            correctValues: ['固']
+        },
+        {
+            strokeCount: 7,
+            correctValues: ['位']
+        },
+        {
+            strokeCount: 8,
+            correctValues: ['泣']
+        },
+        {
+            strokeCount: 7,
+            correctValues: ['労']
+        },
+        {
+            strokeCount: 9,
+            correctValues: ['栄']
+        },
+        {
+            strokeCount: 8,
+            correctValues: ['松']
+        },
+        {
+            strokeCount: 10,
+            correctValues: ['梅']
+        },
+        {
+            strokeCount: 10,
+            correctValues: ['案']
+        },
+        {
+            strokeCount: 11,
+            correctValues: ['巣']
+        },
+        {
+            strokeCount: 11,
+            correctValues: ['健']
+        },
+        {
+            strokeCount: 13,
+            correctValues: ['働']
+        },
+        {
+            strokeCount: 9,
+            correctValues: ['型']
+        },
+        {
+            strokeCount: 11,
+            correctValues: ['堂']
+        },
+        {
+            strokeCount: 11,
+            correctValues: ['械']
+        },
+        {
+            strokeCount: 12,
+            correctValues: ['極']
+        },
+        {
+            strokeCount: 9,
+            correctValues: ['飛']
+        },
+        {
+            strokeCount: 10,
+            correctValues: ['航']
+        },
+        {
+            strokeCount: 10,
+            correctValues: ['特']
+        },
+        {
+            strokeCount: 11,
+            correctValues: ['得']
+        },
+        {
+            strokeCount: 11,
+            correctValues: ['敗']
+        },
+        {
+            strokeCount: 12,
+            correctValues: ['散']
+        },
+        {
+            strokeCount: 13,
+            correctValues: ['節']
+        },
+        {
+            strokeCount: 14,
+            correctValues: ['管']
+        },
+        {
+            strokeCount: 14,
+            correctValues: ['説']
+        },
+        {
+            strokeCount: 15,
+            correctValues: ['課']
+        },
+        {
+            strokeCount: 16,
+            correctValues: ['録']
+        },
+        {
+            strokeCount: 19,
+            correctValues: ['鏡']
+        },
+        {
+            strokeCount: 12,
+            correctValues: ['無']
+        },
+        {
+            strokeCount: 13,
+            correctValues: ['照']
+        },
+        {
+            strokeCount: 14,
+            correctValues: ['種']
+        },
+        {
+            strokeCount: 16,
+            correctValues: ['積']
+        },
+        {
+            strokeCount: 18,
+            correctValues: ['類']
+        },
+        {
+            strokeCount: 19,
+            correctValues: ['願']
+        }
+    ],
+    [
+        {
+            strokeCount: 5,
+            correctValues: ['可']
+        },
+        {
+            strokeCount: 6,
+            correctValues: ['因']
+        },
+        {
+            strokeCount: 5,
+            correctValues: ['句']
+        },
+        {
+            strokeCount: 6,
+            correctValues: ['舌']
+        },
+        {
+            strokeCount: 5,
+            correctValues: ['示']
+        },
+        {
+            strokeCount: 7,
+            correctValues: ['余']
+        },
+        {
+            strokeCount: 5,
+            correctValues: ['刊']
+        },
+        {
+            strokeCount: 7,
+            correctValues: ['判']
+        },
+        {
+            strokeCount: 7,
+            correctValues: ['技']
+        },
+        {
+            strokeCount: 8,
+            correctValues: ['枝']
+        },
+        {
+            strokeCount: 8,
+            correctValues: ['述']
+        },
+        {
+            strokeCount: 9,
+            correctValues: ['迷']
+        },
+        {
+            strokeCount: 8,
+            correctValues: ['制']
+        },
+        {
+            strokeCount: 9,
+            correctValues: ['則']
+        },
+        {
+            strokeCount: 9,
+            correctValues: ['逆']
+        },
+        {
+            strokeCount: 10
+            'correctValues: ['造']
+        },
+        {
+            strokeCount: 9,
+            correctValues: ['限']
+        },
+        {
+            strokeCount: 11
+            'correctValues: ['眼']
+        },
+        {
+            strokeCount: 11
+            'correctValues: ['貧']
+        },
+        {
+            strokeCount: 12
+            'correctValues: ['賀']
+        },
+        {
+            strokeCount: 11
+            'correctValues: ['険']
+        },
+        {
+            strokeCount: 12
+            'correctValues: ['検']
+        },
+        {
+            strokeCount: 11
+            'correctValues: ['設']
+        },
+        {
+            strokeCount: 12
+            'correctValues: ['評']
+        },
+        {
+            strokeCount: 11
+            'correctValues: ['接']
+        },
+        {
+            strokeCount: 12
+            'correctValues: ['提']
+        },
+        {
+            strokeCount: 11
+            'correctValues: ['移']
+        },
+        {
+            strokeCount: 12
+            'correctValues: ['程']
+        },
+        {
+            strokeCount: 12
+            'correctValues: ['富']
+        },
+        {
+            strokeCount: 13
+            'correctValues: ['豊']
+        },
+        {
+            strokeCount: 13
+            'correctValues: ['鉱']
+        },
+        {
+            strokeCount: 14
+            'correctValues: ['銭']
+        },
+        {
+            strokeCount: 13
+            'correctValues: ['預']
+        },
+        {
+            strokeCount: 14
+            'correctValues: ['領']
+        },
+        {'strokeCount': 14, 'correctValues': ['製']},
+        {'strokeCount': 15, 'correctValues': ['導']},
+        {'strokeCount': 15, 'correctValues': ['潔']},
+        {'strokeCount': 16, 'correctValues': ['燃']},
+        {'strokeCount': 18, 'correctValues': ['職']},
+        {'strokeCount': 19, 'correctValues': ['識']}
+    ],
+    [
+        {
+            strokeCount: 4,
+            correctValues: ['収']
+        },
+        {
+            strokeCount: 5,
+            correctValues: ['処']
+        },
+        {
+            strokeCount: 5,
+            correctValues: ['冊']
+        },
+        {
+            strokeCount: 6,
+            correctValues: ['危']
+        },
+        {
+            strokeCount: 6,
+            correctValues: ['吸']
+        },
+        {
+            strokeCount: 8,
+            correctValues: ['呼']
+        },
+        {
+            strokeCount: 7,
+            correctValues: ['卵']
+        },
+        {
+            strokeCount: 8,
+            correctValues: ['乳']
+        },
+        {
+            strokeCount: 8,
+            correctValues: ['沿']
+        },
+        {
+            strokeCount: 9,
+            correctValues: ['洗']
+        },
+        {
+            strokeCount: 9,
+            correctValues: ['背']
+        },
+        {
+            strokeCount: 10,
+            correctValues: ['骨']
+        },
+        {
+            strokeCount: 9,
+            correctValues: ['肺']
+        },
+        {
+            strokeCount: 10,
+            correctValues: ['胸']
+        },
+        {
+            strokeCount: 9,
+            correctValues: ['皇']
+        },
+        {
+            strokeCount: 10,
+            correctValues: ['陛']
+        },
+        {
+            strokeCount: 10,
+            correctValues: ['蚕']
+        },
+        {
+            strokeCount: 13,
+            correctValues: ['絹']
+        },
+        {
+            strokeCount: 11,
+            correctValues: ['異']
+        },
+        {
+            strokeCount: 12,
+            correctValues: ['策']
+        },
+        {
+            strokeCount: 11,
+            correctValues: ['著']
+        },
+        {
+            strokeCount: 13,
+            correctValues: ['署']
+        },
+        {
+            strokeCount: 12,
+            correctValues: ['痛']
+        },
+        {
+            strokeCount: 13,
+            correctValues: ['傷']
+        },
+        {
+            strokeCount: 11,
+            correctValues: ['盛']
+        },
+        {
+            strokeCount: 13,
+            correctValues: ['盟']
+        },
+        {
+            strokeCount: 10,
+            correctValues: ['除']
+        },
+        {
+            strokeCount: 11,
+            correctValues: ['捨']
+        },
+        {
+            strokeCount: 14,
+            correctValues: ['穀']
+        },
+        {
+            strokeCount: 15,
+            correctValues: ['蔵']
+        },
+        {
+            strokeCount: 15,
+            correctValues: ['潮']
+        },
+        {
+            strokeCount: 16,
+            correctValues: ['樹']
+        },
+        {
+            strokeCount: 16,
+            correctValues: ['操']
+        },
+        {
+            strokeCount: 18,
+            correctValues: ['臨']
+        },
+        {'strokeCount': 17, 'correctValues': ['優']},
+        {'strokeCount': 18, 'correctValues': ['簡']},
+        {'strokeCount': 17, 'correctValues': ['厳']},
+        {'strokeCount': 18, 'correctValues': ['難']},
+        {'strokeCount': 16, 'correctValues': ['奮']},
+        {'strokeCount': 19, 'correctValues': ['警']} 
+    ],
+    [
+    ],
+    [
+    ],
+    [
+    ],
+    [
+    ],
+    [
+    ],
+    [
+    ],
 ];
   
